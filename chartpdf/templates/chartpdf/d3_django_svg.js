9b3cf6bcedb1cf3978537b293ce1af71c967ac2d
Jonathan Tremesaygues <jonathan.tremesaygues@sereema.com>
2023-02-02 11:38:57 +0100

    mutualize js part between d3 front and d3 django

--- chartpdf/templates/chartpdf/d3_django_svg.js
+++ chartpdf/templates/chartpdf/d3_django_svg.js
@@ -1,27 +1,9 @@
+{% load inline_static_tags %}
+
 const jsdom = await import("jsdom")
 const d3 = await import("d3")
 
-
-function cp_scatter_with_points(svg, points) {
-    svg.attr("viewBox", [0, 0, 800, 600])
-
-    // Add debug background
-    svg.append("rect",)
-        .attr("width", 800)
-        .attr("height", 600)
-        .attr("fill", "magenta")
-
-    // Draw the points
-    svg.selectAll(".cf-point")
-        .data(points)
-        .join("circle")
-        .attr("cx", p => p.x * 800)
-        .attr("cy", p => p.y * 600)
-        .attr("r", 4)
-        .attr("fill", "green")
-
-    return svg
-}
+{% inline_javascript "chartpdf/cp_scatter.js" %}
 
 const dom = new jsdom.JSDOM(`<svg width="800" height="600"></svg>`);
 let svg = d3.select(dom.window.document).select("svg")
--- proj/settings.py
+++ proj/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "compressor",
+    "inline_static",
     "chartpdf",
 ]
 
